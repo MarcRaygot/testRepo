2da58edf7d100093c37d9657e612559267629052
MarcRaygot <marc.raygot@gmail.com>
2017-02-06 17:51:25 +0100

    Create montest.cs

---
+++ montest.cs
@@ -0,0 +1,9 @@
+ferenceDate, new Calendar(), dayCounter, jumps, jumpDates)
+        {
+
+            _instruments_ = instruments;
+
+            accuracy_ = accuracy;
+            interpolator_ = i;
+            bootstrap_ = bootstrap;
+            _traits_ = new Traits();
